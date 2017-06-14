c0f6d2eeecd2cc64abbcba12922835993a2fa889
magmahindenburg <emil@moar.se>
2017-06-14 16:14:51 +0900

    Fixed external click for ads

--- src/js/controllers/buyandsellController.js
+++ src/js/controllers/buyandsellController.js
@@ -1,9 +1,13 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('buyandsellController', function($scope, $ionicHistory, buyAndSellService, lodash) {
+angular.module('copayApp.controllers').controller('buyandsellController', function($scope, $ionicHistory, buyAndSellService, lodash, externalLinkService) {
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
     $scope.services = buyAndSellService.get();
+
+    $scope.openExternalLink = function(url) {
+      externalLinkService.open(url);
+    }
 
     $scope.$on("$ionicView.enter", function(event, data) {
         console.log("Enter buy page");
@@ -14,8 +18,17 @@ angular.module('copayApp.controllers').controller('buyandsellController', functi
             console.log(r.responseText);
             var adResponse = JSON.parse(r.responseText)
             document.getElementById("exchange-logo").setAttribute('src', "https://www.bitcoin.com" + adResponse.image)
+            document.getElementById("exchange-link").setAttribute('href', "https://www.bitcoin.com" + adResponse.url)
+            document.getElementById("exchange-link").setAttribute('ng-click', "openExternalLink('"+ "https://www.bitcoin.com" + adResponse.url +"')")
             document.getElementById("exchange-name").textContent = adResponse.bannerName;
             document.getElementById("exchange-text").textContent = adResponse.description;
+
+            var exchangeLink = document.getElementById("exchange-link");   
+
+            exchangeLink.addEventListener('click', function() {
+                externalLinkService.open("https://www.bitcoin.com" + adResponse.url);
+            });
+
         };
         r.send("defaultpromotion=360");
     });
