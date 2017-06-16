5b55650bd33b09dd4af7ab56abb0d9699c69b563
magmahindenburg <emil@moar.se>
2017-06-16 14:52:07 +0900

    Updated content

--- src/js/controllers/onboarding/collectEmail.js
+++ src/js/controllers/onboarding/collectEmail.js
@@ -5,7 +5,7 @@ angular.module('copayApp.controllers').controller('collectEmailController', func
   var wallet, walletId;
   $scope.data = {};
   // Get more info: https://mashe.hawksey.info/2014/07/google-sheets-as-a-database-insert-with-apps-script-using-postget-methods-with-ajax-example/
-  var URL = "https://script.google.com/macros/s/AKfycbwQXvUw6-Ix0cRLMi7hBB8dlgNTCTgwfNIQRds6RypPV7dO8evW/exec";
+  var URL = "https://script.google.com/macros/s/1ihZK47nRXHV8O8sA4VFKkRV8wB9iI-l4DwPOWAxIaJs/exec";
 
   var _post = function(dataSrc) {
     return {
