983644327fc926f451517d343b53a42e258ddd45
magmahindenburg <emil@moar.se>
2017-06-18 20:00:54 +0900

    Use blockchain.info as lock explorer for mainnet

--- src/js/controllers/tx-details.js
+++ src/js/controllers/tx-details.js
@@ -161,7 +161,10 @@ angular.module('copayApp.controllers').controller('txDetailsController', functio
 
   $scope.viewOnBlockchain = function() {
     var btx = $scope.btx;
-    var url = 'https://' + ($scope.getShortNetworkName() == 'test' ? 'test-' : '') + 'insight.bitpay.com/tx/' + btx.txid;
+    var url = 'https://blockchain.info/tx/' + btx.txid;
+    if ($scope.getShortNetworkName() == 'test') {
+        url = "https://test-insight.bitpay.com/tx/" + btx.txid;
+    }
     var optIn = true;
     var title = null;
     var message = gettextCatalog.getString('View Transaction on Insight');
