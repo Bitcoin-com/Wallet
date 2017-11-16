36756f2814c4bcb0d9d1988b0d1b6b26f857f296
Kadir Sekha <sekhakadir@gmail.com>
2017-11-16 13:39:45 +0900

    changed ios store bitcoin.com wallet url

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -45,7 +45,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
         wp: ''
       },
       bitcoincom: {
-        ios: 'http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=951330296&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8',
+        ios: 'http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=1252903728&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8',
         android: 'https://play.google.com/store/apps/details?id=com.bitcoin.mwallet',
         wp: ''
       }
