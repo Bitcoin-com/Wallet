4df7b245a40d0a4b8acb88917b57bb3a463bfe40
Kadir Sekha <sekhakadir@gmail.com>
2018-01-30 17:01:06 -0400

    alt currency list now puts the top 10 fiat and recently used at top

--- src/js/controllers/preferencesAltCurrency.js
+++ src/js/controllers/preferencesAltCurrency.js
@@ -6,11 +6,30 @@ angular.module('copayApp.controllers').controller('preferencesAltCurrencyControl
     var next = 10;
     var completeAlternativeList = [];
 
+    var popularCurrencyList = [
+      {isoCode: 'USD', order: 0},
+      {isoCode: 'EUR', order: 1},
+      {isoCode: 'JPY', order: 2},
+      {isoCode: 'GBP', order: 3},
+      {isoCode: 'AUD', order: 4},
+      {isoCode: 'CAD', order: 5},
+      {isoCode: 'CHF', order: 6},
+      {isoCode: 'CNY', order: 7},
+      {isoCode: 'KRW', order: 8},
+      {isoCode: 'HKD', order: 9},
+    ]
+
     function init() {
       var unusedCurrencyList = [{
         isoCode: 'LTL'
       }, {
         isoCode: 'BTC'
+      }, {
+        isoCode: 'BCC'
+      }, {
+        isoCode: 'BCH_BTC'
+      }, {
+        isoCode: 'BCH'
       }];
       rateService.whenAvailable(function() {
 
@@ -18,12 +37,20 @@ angular.module('copayApp.controllers').controller('preferencesAltCurrencyControl
 
         var idx = lodash.indexBy(unusedCurrencyList, 'isoCode');
         var idx2 = lodash.indexBy($scope.lastUsedAltCurrencyList, 'isoCode');
+        var idx3 = lodash.indexBy(popularCurrencyList, 'isoCode');
+        var alternatives = rateService.listAlternatives(true);
 
-        completeAlternativeList = lodash.reject(rateService.listAlternatives(true), function(c) {
-          return idx[c.isoCode] || idx2[c.isoCode];
+        lodash.each(alternatives, function(c) {
+          if (idx3[c.isoCode]) {
+              idx3[c.isoCode].name = c.name;
+          }
+          if (!idx[c.isoCode] && !idx2[c.isoCode] && !idx3[c.isoCode]) {
+            completeAlternativeList.push(c);
+          }
         });
-
+      
         $scope.altCurrencyList = completeAlternativeList.slice(0, 10);
+        $scope.lastUsedPopularList = lodash.unique(lodash.union($scope.lastUsedAltCurrencyList, popularCurrencyList), 'isoCode');
 
         $timeout(function() {
           $scope.$apply();
