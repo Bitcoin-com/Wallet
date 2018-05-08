1ec2bf7ba09264cae9cd41b231cae78d07e397a0
Jean-Baptiste Dominguez <jb@bitcoin.com>
2018-05-08 16:44:55 +0900

    Fix - Search with popularCurrencyList as well

--- src/js/controllers/amount.js
+++ src/js/controllers/amount.js
@@ -517,103 +517,104 @@ angular.module('copayApp.controllers').controller('amountController', function($
   // Currency
 
   var next = 10;
-    var completeAlternativeList = [];
+  var completeAlternativeList = [];
 
-    var popularCurrencyList = [
-      {isoCode: 'USD', order: 0},
-      {isoCode: 'EUR', order: 1},
-      {isoCode: 'JPY', order: 2},
-      {isoCode: 'GBP', order: 3},
-      {isoCode: 'AUD', order: 4},
-      {isoCode: 'CAD', order: 5},
-      {isoCode: 'CHF', order: 6},
-      {isoCode: 'CNY', order: 7},
-      {isoCode: 'KRW', order: 8},
-      {isoCode: 'HKD', order: 9},
-    ]
+  var popularCurrencyList = [
+    {isoCode: 'USD', order: 0},
+    {isoCode: 'EUR', order: 1},
+    {isoCode: 'JPY', order: 2},
+    {isoCode: 'GBP', order: 3},
+    {isoCode: 'AUD', order: 4},
+    {isoCode: 'CAD', order: 5},
+    {isoCode: 'CHF', order: 6},
+    {isoCode: 'CNY', order: 7},
+    {isoCode: 'KRW', order: 8},
+    {isoCode: 'HKD', order: 9},
+  ]
 
-    function initCurrencies() {
-      var unusedCurrencyList = [{
-        isoCode: 'LTL'
-      }, {
-        isoCode: 'BTC'
-      }, {
-        isoCode: 'BCC'
-      }, {
-        isoCode: 'BCH_BTC'
-      }, {
-        isoCode: 'BCH'
-      }];
-      rateService.whenAvailable(function() {
+  function initCurrencies() {
+    var unusedCurrencyList = [{
+      isoCode: 'LTL'
+    }, {
+      isoCode: 'BTC'
+    }, {
+      isoCode: 'BCC'
+    }, {
+      isoCode: 'BCH_BTC'
+    }, {
+      isoCode: 'BCH'
+    }];
+    rateService.whenAvailable(function() {
 
-        $scope.listComplete = false;
+      $scope.listComplete = false;
 
-        var idx = lodash.indexBy(unusedCurrencyList, 'isoCode');
-        var idx2 = lodash.indexBy($scope.lastUsedAltCurrencyList, 'isoCode');
-        var idx3 = lodash.indexBy(popularCurrencyList, 'isoCode');
-        var alternatives = rateService.listAlternatives(true);
+      var idx = lodash.indexBy(unusedCurrencyList, 'isoCode');
+      var idx2 = lodash.indexBy($scope.lastUsedAltCurrencyList, 'isoCode');
+      var idx3 = lodash.indexBy(popularCurrencyList, 'isoCode');
+      var alternatives = rateService.listAlternatives(true);
 
-        lodash.each(alternatives, function(c) {
-          if (idx3[c.isoCode]) {
-              idx3[c.isoCode].name = c.name;
-          }
-          if (!idx[c.isoCode] && !idx2[c.isoCode] && !idx3[c.isoCode]) {
-            completeAlternativeList.push(c);
-          }
-        });
-
-        $scope.altCurrencyList = completeAlternativeList.slice(0, 10);
-        $scope.lastUsedPopularList = lodash.unique(lodash.union($scope.lastUsedAltCurrencyList, popularCurrencyList), 'isoCode');
-
-        $timeout(function() {
-          $scope.$apply();
-        });
+      lodash.each(alternatives, function(c) {
+        if (idx3[c.isoCode]) {
+            idx3[c.isoCode].name = c.name;
+        }
+        if (!idx[c.isoCode] && !idx2[c.isoCode] && !idx3[c.isoCode]) {
+          completeAlternativeList.push(c);
+        }
       });
-    }
 
-    $scope.loadMore = function() {
-      $timeout(function() {
-        $scope.altCurrencyList = completeAlternativeList.slice(0, next);
-        next += 10;
-        $scope.listComplete = $scope.altCurrencyList.length >= completeAlternativeList.length;
-        $scope.$broadcast('scroll.infiniteScrollComplete');
-      }, 100);
-    };
+      $scope.altCurrencyList = completeAlternativeList.slice(0, 10);
+      $scope.lastUsedPopularList = lodash.unique(lodash.union($scope.lastUsedAltCurrencyList, popularCurrencyList), 'isoCode');
 
-    $scope.findCurrency = function(search) {
-      if (!search) initCurrencies();
-      $scope.altCurrencyList = lodash.filter(completeAlternativeList, function(item) {
-        var val = item.name
-        var val2 = item.isoCode;
-        return lodash.includes(val.toLowerCase(), search.toLowerCase()) || lodash.includes(val2.toLowerCase(), search.toLowerCase());
-      });
       $timeout(function() {
         $scope.$apply();
       });
-    };
+    });
+  }
 
-    $scope.save = function(newAltCurrency) {
-      var opts = {
-        wallet: {
-          settings: {
-            alternativeName: newAltCurrency.name,
-            alternativeIsoCode: newAltCurrency.isoCode,
-          }
+  $scope.loadMore = function() {
+    $timeout(function() {
+      $scope.altCurrencyList = completeAlternativeList.slice(0, next);
+      next += 10;
+      $scope.listComplete = $scope.altCurrencyList.length >= completeAlternativeList.length;
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    }, 100);
+  };
+
+  $scope.findCurrency = function(search) {
+    if (!search) initCurrencies();
+    var list = lodash.unique(lodash.union(completeAlternativeList, lodash.union($scope.lastUsedAltCurrencyList, popularCurrencyList)), 'isoCode');
+    $scope.altCurrencyList = lodash.filter(list, function(item) {
+      var val = item.name
+      var val2 = item.isoCode;
+      return lodash.includes(val.toLowerCase(), search.toLowerCase()) || lodash.includes(val2.toLowerCase(), search.toLowerCase());
+    });
+    $timeout(function() {
+      $scope.$apply();
+    });
+  };
+
+  $scope.save = function(newAltCurrency) {
+    var opts = {
+      wallet: {
+        settings: {
+          alternativeName: newAltCurrency.name,
+          alternativeIsoCode: newAltCurrency.isoCode,
         }
-      };
-
-      configService.set(opts, function(err) {
-        if (err) $log.warn(err);
-        walletService.updateRemotePreferences(profileService.getWallets());
-        var altUnitIndex = lodash.findIndex(availableUnits, {
-          isFiat: true
-        });
-        availableUnits[altUnitIndex].id = newAltCurrency.isoCode;
-        availableUnits[altUnitIndex].name = newAltCurrency.isoCode;
-        availableUnits[altUnitIndex].shortName = newAltCurrency.isoCode;
-        fiatCode = newAltCurrency.isoCode;
-        updateUnitUI();
-        $scope.close();
-      });
+      }
     };
+
+    configService.set(opts, function(err) {
+      if (err) $log.warn(err);
+      walletService.updateRemotePreferences(profileService.getWallets());
+      var altUnitIndex = lodash.findIndex(availableUnits, {
+        isFiat: true
+      });
+      availableUnits[altUnitIndex].id = newAltCurrency.isoCode;
+      availableUnits[altUnitIndex].name = newAltCurrency.isoCode;
+      availableUnits[altUnitIndex].shortName = newAltCurrency.isoCode;
+      fiatCode = newAltCurrency.isoCode;
+      updateUnitUI();
+      $scope.close();
+    });
+  };  
 });
