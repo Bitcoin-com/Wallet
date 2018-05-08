f945b0f9a75b959b343166becb6536c8d6cd2043
Sebastiaan Pasma <sebastiaan@enkodo.nl>
2018-05-08 10:28:13 +0200

    Remember the state of collapsed/open menus on home tab

--- src/js/controllers/homeIntegrations.js
+++ src/js/controllers/homeIntegrations.js
@@ -1,8 +1,12 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('homeIntegrationsController', function($scope, homeIntegrationsService, $ionicScrollDelegate, $timeout) {
-
+angular.module('copayApp.controllers').controller('homeIntegrationsController', function($scope, homeIntegrationsService, $ionicScrollDelegate, $timeout, configService) {
   $scope.hide = false;
+
+  configService.whenAvailable(function(config) {
+    $scope.hide = config.homeSectionIsHidden&&config.homeSectionIsHidden['homeIntegrations']?config.homeSectionIsHidden['homeIntegrations']:false;
+  });
+
   $scope.services = homeIntegrationsService.get();
 
   $scope.toggle = function() {
@@ -10,6 +14,9 @@ angular.module('copayApp.controllers').controller('homeIntegrationsController', 
     $timeout(function() {
       $ionicScrollDelegate.resize();
       $scope.$apply();
+      configService.set({homeSectionIsHidden: {homeIntegrations: $scope.hide}}, function(err) {
+        if (err) $log.debug(err);
+      });
     }, 10);
   };
 
