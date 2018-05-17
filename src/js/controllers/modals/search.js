01b1c6753fc17b2412bc110a8a29150ff2cc21a6
Jean-Baptiste Dominguez <jb@bitcoin.com>
2018-05-17 20:16:41 +0900

    Fix - 321 - Keep only the fetching in the outputs

--- src/js/controllers/modals/search.js
+++ src/js/controllers/modals/search.js
@@ -33,18 +33,7 @@ angular.module('copayApp.controllers').controller('searchController', function($
         if ($scope.wallet.coin === 'bch') {
 
           /**
-           * One tx in JSON
-           * {"txid":"97ed105ea5042a328b68da43439b4..","action":"received","amount":730216,"fees":392,"time":1525661853,"confirmations":1459,"feePerKb":1074,"outputs":[{"amount":730216,"address":"19zA4aP1sAavtHF2wJcMpi..","message":null}],"message":null,"creatorName":"","hasUnconfirmedInputs":false,"amountStr":"0.00730216 BCH","alternativeAmountStr":"7.95 EUR","feeStr":"0.00000392 BCH","amountValueStr":"0.00730216","amountUnitStr":"BCH","safeConfirmed":"6+","lowAmount":false}
-           * These two lines should be removed.. because tx.addressTo does not exist.
-           * The address is in tx.outputs[..].address, cf. the JSON
-           */
-          if (addressTo) {
-            var addr = bitcoinCashJsService.translateAddresses(addressTo);
-            addressTo = addr.legacy + addr.bitpay + 'bitcoincash:' + addr.cashaddr
-          }
-
-          /**
-           * For each address (normally only one)
+           * For each address
            * I translate the legacy address and add in the searchable string the 3 kind of addresses
            */
           lodash.each(tx.outputs, function(output) {
