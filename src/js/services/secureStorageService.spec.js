f4642b1953c4c52c959ee9d45b0d090553f83d75
Brendon Duncan <brendon@bitcoin.com>
2018-06-08 16:19:50 +1200

    All tests for secure storage service on desktop.

--- src/js/services/secureStorageService.spec.js
+++ src/js/services/secureStorageService.spec.js
@@ -17,7 +17,6 @@ describe('secureStorageService on desktop', function(){
         
     module(function($provide) {
       $provide.value('desktopSecureStorageService', desktopSss);
-      //$provide.value('$log', log); // Handy for debugging test failures
       $provide.value('platformInfo', platformInfoStub);
     });
 
@@ -25,6 +24,24 @@ describe('secureStorageService on desktop', function(){
       sss = $injector.get('secureStorageService');
     });
 
+  });
+
+  it('get fails', function() {
+    var error, key, result;
+
+    desktopSss.get.and.callFake(function(k, cb){
+      key = k;
+      cb(new Error('Get error.'), null);
+    });
+
+    sss.get('a1234', function(e, res) {
+      error = e;
+      result = res;
+    });
+
+    expect(error.message).toBe('Get error.');
+    expect(result).toBeFalsy();
+    expect(key).toBe('a1234');
   });
 
   it('get succeeds', function() {
@@ -43,6 +60,42 @@ describe('secureStorageService on desktop', function(){
     expect(error).toBeFalsy();
     expect(result).toBe('The result 1.');
     expect(key).toBe('a123');
+  });
+
+  it('set fails', function() {
+    var error, key, value;
+
+    desktopSss.set.and.callFake(function(k, v, cb){
+      key = k;
+      value = v;
+      cb(new Error('Set error.'));
+    });
+
+    sss.set('a12345', 'The value 1.', function(e) {
+      error = e;
+    });
+
+    expect(error.message).toBe('Set error.');
+    expect(key).toBe('a12345');
+    expect(value).toBe('The value 1.');
+  });
+
+  it('set succeeds', function() {
+    var error, key, value;
+
+    desktopSss.set.and.callFake(function(k, v, cb){
+      key = k;
+      value = v;
+      cb(null);
+    });
+
+    sss.set('ab123', 'The value 2.', function(e) {
+      error = e;
+    });
+
+    expect(error).toBeFalsy();
+    expect(key).toBe('ab123');
+    expect(value).toBe('The value 2.')
   });
 
 });
