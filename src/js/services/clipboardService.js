187199230377798c3cd40255f54933e58ca03b2e
Sebastiaan Pasma <sebastiaan@enkodo.nl>
2018-07-16 17:42:15 +0200

    Use native navigator clipboard over element data clipboard function

--- src/js/services/clipboardService.js
+++ src/js/services/clipboardService.js
@@ -11,10 +11,15 @@ angular.module('copayApp.services').factory('clipboardService', function ($http,
       cordova.plugins.clipboard.copy(data);
     } else if (platformInfo.isNW) {
       nodeWebkitService.writeToClipboard(data);
+    } else if (navigator && navigator.clipboard) {
+      $log.debug("Use navigator clipboard.")
+      navigator.clipboard.writeText(data).catch(err => {
+        $log.debug("Clipboard writing is not supported in your browser..");
+      });
     } else if (clipboard.supported) {
       clipboard.copyText(data);
     } else {
-      // No supported
+      // Not supported
       return;
     }
 
