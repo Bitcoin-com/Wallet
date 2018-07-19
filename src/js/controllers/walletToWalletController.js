c3cded5cb06ca88e32b50d5b33dbf583e450fbba
Sebastiaan Pasma <sebastiaan@enkodo.nl>
2018-07-19 15:50:09 +0200

    wallet to wallet (sub)title

--- src/js/controllers/walletToWalletController.js
+++ src/js/controllers/walletToWalletController.js
@@ -2,6 +2,10 @@
 
 angular.module('copayApp.controllers').controller('walletToWalletController', function($scope, $rootScope, $log, profileService, configService) {
 
+  // TODO: change according to which screen this is, origin/destination
+  $scope.headerTitle = gettextCatalog.getString('Choose your origin wallet');
+  $scope.headerSubtitle = gettextCatalog.getString('This is where the Bitcoin will be taken out from.');
+
   $scope.$on("$ionicView.enter", function(event, data) {
     $scope.walletsBch = profileService.getWallets({coin: 'bch'});
     $scope.walletsBtc = profileService.getWallets({coin: 'btc'});
