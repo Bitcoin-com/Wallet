eb051a22d31469a8302198fbb0a1d9c84a34272e
Brendon Duncan <brendon@bitcoin.com>
2018-10-11 17:47:43 +1300

    Moonpay service.

---
+++ src/js/services/buy-bitcoin/moonpay.service.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function(){
+
+angular
+  .module('bitcoincom.services')
+  .factory('moonpayService', moonpayService);
+  
+  function moonpayService() {
+
+    var service = {
+      
+    };
+
+    return service;
+  };
+
+})();
