2a670aba21167b895086b49ba4ce853f5e4608ca
Brendon Duncan <brendon@bitcoin.com>
2018-10-11 17:51:43 +1300

    goBack() on Get Verified screen.

--- src/js/controllers/buy-bitcoin/kyc-get-verified.controller.js
+++ src/js/controllers/buy-bitcoin/kyc-get-verified.controller.js
@@ -6,14 +6,14 @@ angular
   .module('bitcoincom.controllers')
   .controller('kycGetVerifiedController', kycGetVerifiedController);
 
-  function kycGetVerifiedController(addressbookService, externalLinkService, bitcoinCashJsService, bitcore, bitcoreCash, bwcError, clipboardService, configService, feeService, gettextCatalog, $interval, $ionicHistory, $ionicModal, ionicToast, lodash, $log, ongoingProcess, platformInfo, popupService, profileService, $scope, sendFlowService, shapeshiftService, soundService, $state, $timeout, txConfirmNotification, txFormatService, walletService) {
+  function kycGetVerifiedController(kycFlowService) {
     console.log('kycGetVerifiedController');
     var vm = this;
 
     vm.goBack = goBack;
 
     function goBack() {
-      
+      kycFlowService.router.goBack();
     }
   }
 
