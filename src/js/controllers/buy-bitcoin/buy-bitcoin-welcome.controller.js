903bacd713b9a830c7ba7418f71b310b5967028e
Brendon Duncan <brendon@bitcoin.com>
2018-10-16 16:48:34 +1300

    Getting the email address from the form.

--- src/js/controllers/buy-bitcoin/buy-bitcoin-welcome.controller.js
+++ src/js/controllers/buy-bitcoin/buy-bitcoin-welcome.controller.js
@@ -6,18 +6,32 @@ angular
   .module('bitcoincom.controllers')
   .controller('buyBitcoinWelcomeController', buyBitcoinWelcomeController);
 
-  function buyBitcoinWelcomeController(kycFlowService) {
+  function buyBitcoinWelcomeController(kycFlowService, $log, $scope) {
     var vm = this;
 
+    // Functions
     vm.getStarted = getStarted;
     vm.goBack = goBack;
 
+    function initVariables() {
+      vm.email = 'bla';
+    }
+
+    $scope.$on("$ionicView.beforeEnter", onBeforeEnter);
+
+
     function getStarted() {
-      
+      $log.debug('getStarted() with email: ' + vm.email);
     }
 
     function goBack() {
       kycFlowService.goBack();
+    }
+
+    
+
+    function onBeforeEnter(event, data) {
+      initVariables();
     }
   }
 
