daddf22c81d141af16d1a06bf277eb16e30f7ead
Brendon Duncan <brendon@bitcoin.com>
2018-10-17 16:15:57 +1300

    Workaround for Cordova not supporting XCode 10.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,13 +21,13 @@ module.exports = function(grunt) {
         command: 'cordova prepare android && cordova build android --release',
       },
       build_ios_debug: {
-        command: 'cordova prepare ios && cordova build ios --debug',
+        command: 'cordova prepare ios && cordova build ios --debug --buildFlag="-UseModernBuildSystem=0"',
         options: {
           maxBuffer: 3200 * 1024
         }
       },
       build_ios_release: {
-        command: 'cordova prepare ios && cordova build ios --release',
+        command: 'cordova prepare ios && cordova build ios --release --buildFlag="-UseModernBuildSystem=0"',
         options: {
           maxBuffer: 3200 * 1024
         }
