287c7985beeb9aea8b3d2ff39c87d0bc1ffb2fd6
Brendon Duncan <brendon@bitcoin.com>
2018-10-31 12:07:26 +1300

    Displayin wallet and payment method values from model, rather than hard-coded values.

--- src/js/controllers/buy-bitcoin/amount.controller.js
+++ src/js/controllers/buy-bitcoin/amount.controller.js
@@ -5,14 +5,59 @@
       .module('bitcoincom.controllers')
       .controller('buyBitcoinAmountController', amountController);
 
-  function amountController($scope) {
+  function amountController(configService, profileService, $scope) {
     var vm = this;
 
     vm.focussedEl = false;
 
-    var controller = {
-      // Functions
-    };
-    return controller;
+    function _initVariables() {
+      vm.displayBalanceAsFiat = true;
+      vm.inputValue = 1;
+      vm.lineItems = {
+        cost: 125,
+        processingFee: 10,
+        total: 13511
+      };
+      vm.paymentMethod = {
+        name: 'Visa',
+        partialCardNumber: '••• 2244',
+        expiryDate: '12/21'
+      };
+      vm.wallet = null;
+
+      configService.whenAvailable(function onConfigService(config){
+
+        vm.displayBalanceAsFiat = config.wallet.settings.priceDisplay === 'fiat';
+        console.log('displayBalanceAsFiat: ' + vm.displayBalanceAsFiat);
+      });
+
+      // TODO: What if no wallet available?
+      if (walletId) {
+        console.log('walletId: "' + walletId +'"');
+        vm.wallet = profileService.getWallet(walletId);
+      } else {
+        var walletOpts = {
+          coin: 'bch'
+        };
+        var wallets = profileService.getWallets(walletOpts);
+        console.log('wallet count: ' + wallets.length);
+        if (wallets.length > 0) {
+          vm.wallet = wallets[0];
+        } else {
+          vm.wallet = null;
+        }
+      }
+    }
+
+    $scope.$on('$ionicView.beforeEnter', _onBeforeEnter);
+
+    var walletId = '693923fb-0554-45a5-838f-6efa26ca917e';
+
+    function _onBeforeEnter() {
+      console.log('_onBeforeEnter()');
+      _initVariables();
+
+    }
+
   }
 })();
