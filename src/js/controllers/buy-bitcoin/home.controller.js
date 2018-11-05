915611560420d2e7131ed4f70e103843e18621c0
Brendon Duncan <brendon@bitcoin.com>
2018-11-05 21:03:12 +1300

    The Home screen now looks like the design.

--- src/js/controllers/buy-bitcoin/home.controller.js
+++ src/js/controllers/buy-bitcoin/home.controller.js
@@ -23,11 +23,20 @@ angular
     vm.goBack = goBack;
     vm.onBuyInstantly = onBuyInstantly;
 
+    // Functions - Short because the appear in translatable strings
+    vm.onPp = onPrivacyPolicy;
+    vm.onTos = onTermsOfService;
+
     function _initVariables() {
-      
+      console.log('home _initVariables()');
+      vm.purchasedAmount = '$100';
+      vm.weeklyLimit = '$1000';
+
+      vm.privacyPolicy = 'tabs.buybitcoin-privacypolicy'
+      vm.termsOfService = 'tabs.buybitcoin-tos'
     }
 
-    $scope.$on("$ionicView.beforeEnter", _onBeforeEnter);
+    $scope.$on('$ionicView.beforeEnter', _onBeforeEnter);
 
 
     function getStarted() {
@@ -43,9 +52,12 @@ angular
     function _onBeforeEnter(event, data) {
       _initVariables();
 
+      //$state.go('tabs.buybitcoin-welcome');
+      //return;
+
       moonPayService.getCustomerId().then(
         function onCustomerIdSuccess(customerId) {
-          console.log('moonpay onCustomerIdSuccess with: ' + customerId);
+          console.log('moonpay onCustomerIdSuccess with: 1 ' + customerId);
           if (customerId) {
             console.log('Found customer ID: ' + customerId);
           } else {
@@ -65,6 +77,14 @@ angular
     function onBuyInstantly() {
       // TODO: Check if have a payment method set up etc
       $state.go('tabs.buybitcoin-amount');
+    }
+
+    function onPrivacyPolicy() {
+      console.log('pp');
+    }
+
+    function onTermsOfService() {
+      console.log('tos');
     }
   }
 
