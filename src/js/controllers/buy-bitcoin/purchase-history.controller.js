f7dcc72d3477c744ea088862d7dab0759f80c8a3
Brendon Duncan <brendon@bitcoin.com>
2018-11-07 20:09:09 +1300

    Dummy list in history.

--- src/js/controllers/buy-bitcoin/purchase-history.controller.js
+++ src/js/controllers/buy-bitcoin/purchase-history.controller.js
@@ -9,16 +9,24 @@
   function purchaseHistoryController($scope) {
     var vm = this;
 
-    vm.focussedEl = false;
+    vm.history = [];
 
     $scope.$on('$ionicView.beforeEnter', _onBeforeEnter);
 
     function _initVariables() {
-      
+      vm.history = [];
+      vm.history.push({
+        walletName: 'sdfd',
+        subtitle: 'subdfd'
+      });
+      vm.history.push({
+        walletName: 'sdfd2',
+        subtitle: 'subdfd2'
+      });
     }
 
     function _onBeforeEnter() {
-      
+      _initVariables();
     }
 
   }
