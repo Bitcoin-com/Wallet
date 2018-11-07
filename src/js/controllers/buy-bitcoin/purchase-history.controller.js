e4a08591d5f1c7297ff0c2430957f51d30e89029
jbdtky <jb@bitcoin.com>
2018-11-07 19:45:52 +0900

    Handle error for the purchase history

--- src/js/controllers/buy-bitcoin/purchase-history.controller.js
+++ src/js/controllers/buy-bitcoin/purchase-history.controller.js
@@ -6,7 +6,10 @@
       .module('bitcoincom.controllers')
       .controller('buyBitcoinPurchaseHistoryController', purchaseHistoryController);
 
-  function purchaseHistoryController($scope) {
+  function purchaseHistoryController(
+    popupService,
+    $scope, $ionicHistory
+  ) {
     var vm = this;
 
     vm.history = [];
@@ -18,6 +21,17 @@
       moonPayService.getTransactions().then(
         function onGetTransactionsSuccess(transactions) {
           vm.history = transactions;
+        },
+        function onGetTransactionsError(err) {
+          var title = gettextCatalog.getString('Error Getting Transaction History');
+          var message = err.message || gettextCatalog.getString('An error occurred when getting your transaction history.');
+          var okText = gettextCatalog.getString('Go Back');
+          popupService.showAlert(title, message, 
+            function onAlertDismissed() {
+              $ionicHistory.goBack();
+            }, 
+            okText
+          );
         }
       );
     }
