56c3dfdf9a8f6ec24cfa0faa03febd6802f909fc
Brendon Duncan <brendon@bitcoin.com>
2018-11-08 00:10:47 +1300

    UI Layout and some functionality for Success screen.

--- src/js/controllers/buy-bitcoin/success.controller.js
+++ src/js/controllers/buy-bitcoin/success.controller.js
@@ -5,19 +5,54 @@
       .module('bitcoincom.controllers')
       .controller('buyBitcoinSuccessController', successController);
 
-  function successController($scope) {
+  function successController(
+    $ionicHistory,
+    $scope,
+    $state,
+    ) {
     var vm = this;
-
+    vm.onDone = onDone;
+    vm.onMakeAnotherPurchase = onMakeAnotherPurchase;
 
     $scope.$on('$ionicView.beforeEnter', _onBeforeEnter);
 
     function _initVariables() {
-      vm.purchasedAmount = 0;
+      vm.purchasedAmount = 0.15384976;
+      vm.purchasedCurrency = 'bch';
+      vm.walletName = 'Business Wallet';
     }
 
     function _onBeforeEnter() {
       console.log('_onBeforeEnter()');
       _initVariables();
+    }
+
+    function onDone() {
+      $ionicHistory.nextViewOptions({
+        disableAnimation: true,
+        historyRoot: true
+      });
+      $state.go('tabs.home').then(
+        function() {
+          $state.go('tabs.buybitcoin');
+        }
+      );
+    }
+
+    function onMakeAnotherPurchase() {
+      $ionicHistory.nextViewOptions({
+        disableAnimation: true,
+        historyRoot: true
+      });
+      $state.go('tabs.home').then(
+        function() {
+          $state.go('tabs.buybitcoin').then(
+            function () {
+              $state.go('tabs.buybitcoin-amount');
+            }
+          );
+        }
+      );
     }
 
   }
