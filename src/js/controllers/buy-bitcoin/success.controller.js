2a4e7abc9909eb9ef9dfee20c6c50a548903151b
Brendon Duncan <brendon@bitcoin.com>
2018-11-08 19:31:57 +1300

    Can go to wallet from Success screen.

--- src/js/controllers/buy-bitcoin/success.controller.js
+++ src/js/controllers/buy-bitcoin/success.controller.js
@@ -6,21 +6,26 @@
       .controller('buyBitcoinSuccessController', successController);
 
   function successController(
-    $ionicHistory,
-    moonPayService,
-    $scope,
-    $state,
+    $ionicHistory
+    , $log
+    , moonPayService
+    , profileService
+    , $scope
+    , $state
     ) {
     var vm = this;
     vm.onDone = onDone;
+    vm.onGoToWallet = onGoToWallet;
     vm.onMakeAnotherPurchase = onMakeAnotherPurchase;
+
+    var walletId = '';
 
     $scope.$on('$ionicView.beforeEnter', _onBeforeEnter);
 
     function _initVariables() {
       vm.purchasedAmount = 0.15384976;
       vm.purchasedCurrency = 'bch';
-      vm.walletName = 'Business Wallet';
+      vm.walletName = '';
     }
 
     function _onBeforeEnter() {
@@ -28,11 +33,21 @@
       _initVariables();
 
       moonPayService.getDefaultWalletId().then(
-        function onGetDefaultWalletIdSuccess(walletId) {
+        function onGetDefaultWalletIdSuccess(id) {
+          if (id) {
+            walletId = id;
+            var wallet = profileService.getWallet(walletId);
+            vm.walletName = wallet.name;
+
+          } else {
+            $log.error('Success screen: Default wallet ID not found.');
+            // Can't do much, leave in unknown wallet state
+          }
 
         },
         function onGetDefaultWalletIdError(err) {
-
+          $log.error(err);
+          // Can't do much, leave in unknown wallet state
         }
       );
     }
@@ -43,8 +58,20 @@
         historyRoot: true
       });
       $state.go('tabs.home').then(
-        function() {
+        function () {
           $state.go('tabs.buybitcoin');
+        }
+      );
+    }
+
+    function onGoToWallet() {
+      $ionicHistory.nextViewOptions({
+        disableAnimation: true,
+        historyRoot: true
+      });
+      $state.go('tabs.home').then(
+        function() {
+          $state.go('tabs.wallet', { walletId: walletId });
         }
       );
     }
