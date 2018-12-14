0f8bc25d0a68aa4975c69ad4e43f548826fa8d10
Brendon Duncan <brendon@bitcoin.com>
2018-12-15 08:39:53 +1300

    Business list.

--- src/js/controllers/tab-spend.controller.js
+++ src/js/controllers/tab-spend.controller.js
@@ -10,10 +10,36 @@
     
     var vm = this;
 
+    // Functions
+    vm.goToMerchant = goToMerchant;
+
+    // Variables
+    vm.merchants = [];
+
+
+    
+
     $scope.$on("$ionicView.beforeEnter", _onBeforeEnter);
+
+    function goToMerchant(index) {
+      var merchant = vm.merchants[index];
+      console.log('goToMerchant() ' + merchant.name);
+    }
 
 
     function _onBeforeEnter(event, data) {
+
+      // Sample data
+      vm.merchants = [
+        {
+          description: 'Bits and bobs.',
+          name: 'Bob\'s Boutique'
+        },
+        {
+          description: 'Everything eclectic.',
+          name: 'Eve\'s Emporium'
+        }
+      ];
     }
   }
   
