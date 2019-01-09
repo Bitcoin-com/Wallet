ea7eb9fb6e27e2ceee559beb923cb7ed0d0d4fcd
jchau207 <jonathan@bitcoin.com>
2019-01-09 02:43:34 -0800

    fixed import script

--- app-template/bitcoincom/import_scheme.sh
+++ app-template/bitcoincom/import_scheme.sh
@@ -2,20 +2,18 @@
 
 # Set to your project and scheme
 
-export PROJECT_FILE="../../platforms/ios/Bitcoin.com Wallet.xcodeproj"
-export SCHEME="BETA.xcscheme"
-export TEST="../../platforms/ios"
+export PROJECT_FILE='../../platforms/ios/Bitcoin.com Wallet.xcodeproj'
+export SCHEME='BETA.xcscheme'
 
 # Generate path to shared schemes folder
-export SCHEMES="$PROJECT_FILE/xcshareddata/xcschemes"
-export DEST="$SCHEMES/$SCHEME"
 
-if [ ! -f "$DEST" ]; then
+export SCHEMES="$PROJECT_FILE"/xcshareddata/xcschemes
+
+if [ ! -f "$SCHEMES/$SCHEME" ]; then
 
     # Create folder if necessary
-    echo making directory $SCHEMES
-    mkdir -p $SCHEMES
+    mkdir -p "$SCHEMES"
 
     # Copy scheme
-    cp "${SCHEME}" "$DEST"
+    cp $SCHEME "$SCHEMES/$SCHEME"
 fi
