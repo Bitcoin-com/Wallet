90e309f1c4822df607f058c101fe8af9959ba760
Brendon Duncan <brendon@bitcoin.com>
2019-01-13 17:43:40 -0600

    Command line parameters for icon text and color.

--- app-template/annotate_icon.sh
+++ app-template/annotate_icon.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+#usage: ./annotate_icon.sh beta BETA 003eaa
+
+# Required command line parameters
 FOLDER_NAME=$1
+ICON_TEXT=$2
+HEX_COLOR=$3
+
 DEST_FOLDER_RELATIVE=../resources/bitcoin.com/ios/icon/$FOLDER_NAME
 
 mkdir $DEST_FOLDER_RELATIVE
-convert '../resources/bitcoin.com/ios/icon/icon-1024.png' -gravity center -pointsize 144 -fill '#003eaa' -draw "fill #003eaa rotate -45 rectangle -500,1035,500,1200"  -draw "fill white font 'bitcoincom/fonts/ProximaNova-ExtraBold.otf'  rotate -45 text 0,400 'DEBUG'" $DEST_FOLDER_RELATIVE/icon-1024.png
+convert '../resources/bitcoin.com/ios/icon/icon-1024.png' -gravity center -pointsize 144 -draw "fill '#$HEX_COLOR' rotate -45 rectangle -500,1035,500,1200"  -draw "fill white font 'bitcoincom/fonts/ProximaNova-ExtraBold.otf'  rotate -45 text 0,400 '$ICON_TEXT'" $DEST_FOLDER_RELATIVE/icon-1024.png
 convert $DEST_FOLDER_RELATIVE/icon-1024.png -resize 120x120 $DEST_FOLDER_RELATIVE/icon-120.png
