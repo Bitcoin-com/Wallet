c368abb2eb2ba5d97638c831f990daed99d8f6e9
Brendon Duncan <brendon@bitcoin.com>
2019-02-13 11:38:04 +1300

    Other part of getting VGS identifier from config.

--- src/js/templates/constants/moonpay-config.constant.js
+++ src/js/templates/constants/moonpay-config.constant.js
@@ -8,13 +8,15 @@ Requires a file above the project root: moonpay-config.json, containing:
   {
     "dev": {
       "baseUrl": "YOUR_DEV_BASE_URL",
-      "pubKey": "YOUR_PROD_API_KEY",
-      "secretKey": "YOUR_PROD_API_KEY"
+      "pubKey": "YOUR_DEV_API_KEY",
+      "secretKey": "YOUR_DEV_API_KEY"
+      "vgsKey": "YOUR_VGS_DEV_KEY"
     },
     "prod": {
       "baseUrl": "YOUR_PROD_BASE_URL",
       "pubKey": "YOUR_PROD_API_KEY",
-      "secretKey": "YOUR_PROD_API_KEY"
+      "secretKey": "YOUR_PROD_API_KEY",
+      "vgsKey": "YOUR_VGS_PROD_KEY"
     }
   }
 */
@@ -25,9 +27,10 @@ Requires a file above the project root: moonpay-config.json, containing:
     .module('bitcoincom.services')
     .constant('moonPayConfig', {
       baseUrl: '',
+      env: '',
       pubKey: '',
       secretKey: '',
-      env: ''
+      vgsKey: '',
     });
     
 })();
