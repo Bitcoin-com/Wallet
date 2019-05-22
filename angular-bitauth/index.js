520f9da4d802ad8b39aaa014170829197994b441
ZeerArin <32914858+ZeerArin@users.noreply.github.com>
2019-05-23 06:02:42 +0700

    Update index.js

--- angular-bitauth/index.js
+++ angular-bitauth/index.js
@@ -16,3 +16,22 @@ bitauthModule.provider("bitauthService", function() {
 
   return provider;
 });
+
+var bitauthModule = angular.module('bitauthModule', []);
+var bitauth = require('../node_modules/bitauth');
+
+bitauthModule.constant('MODULE_VERSION', '1.0.0');
+
+bitauthModule.provider("bitauthService", function() {
+  var provider = {};
+
+  provider.$get = function() {
+    var service = {};
+
+    service = bitauth;
+
+    return service;
+  };
+
+  return provider;
+});
